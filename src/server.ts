5e3fe0c14161fc188c5c0aa3e35480332a92b7c7
Fernando Afonso <afonsofernando051@gmail.com>
2022-05-25 12:02:07 -0300

    first commit

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
+import  path  from 'path';
 import express, { request, response } from "express";
 import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-app.use(routes)
+app.use("/uploads", express.static(path.resolve(__dirname,"..","uploads")))
 
-app.listen(3000);
+app.listen(3010);
